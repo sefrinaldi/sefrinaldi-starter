b2b251deeaeab1226e2910c5eba4ac3c987e811a
thomson470@gmail.com <thomson470@gmail.com>
2023-02-21 11:27:48 +0700

    first commit

---
+++ src/main/java/com/sefrinaldi/sefrinaldistarter/SefrinaldiStarterApplication.java
@@ -0,0 +1,13 @@
+package com.sefrinaldi.sefrinaldistarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SefrinaldiStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SefrinaldiStarterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sefrinaldi/sefrinaldistarter/SefrinaldiStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sefrinaldi.sefrinaldistarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SefrinaldiStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
